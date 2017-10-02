e1d0ad8239bc80ed0df726ca410ef76e6f3e76ed
Osnizle <osnizlesniz@gmail.com>
2017-10-01 22:31:15 -0700

    Fixed Wrong Password site

--- WPass/sketch.js
+++ WPass/sketch.js
@@ -1,11 +1,8 @@
 /* Code made by Aiden Onstott/Osnott
 DO NOT COPY WITHOUT CREDIT PLEASE!!! */
-var button;
 
 function setup() {
   noCanvas();
-  button = createButton("Retry");
-  button.mousePressed(GoBack);
 }
 
 function GoBack() {
