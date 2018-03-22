9d6382db083c2b931a4ec736b2aa37619879b3c2
Osnizle <osnizlesniz@gmail.com>
2018-03-21 21:14:58 -0700

    Added Video

--- ReDir/ImageRecognition/sketch2.js
+++ ReDir/ImageRecognition/sketch2.js
@@ -1,3 +1,27 @@
 function goBack() {
   window.location.href = window.location.href.replace("how-does-it-work.html", " ");
 }
+var idleTime = 0;
+document.getElementById('video').onended = function() {
+  console.log("Finnished Video");
+  var idleInterval = setInterval(timerIncrement, 2000); // 2 seconds
+
+  //Zero the idle timer on mouse movement.
+  $(document).mousemove(function (e) {
+      idleTime = 0;
+      console.log("Mousemove");
+      $(".ruthere").css("display", "none");
+  });
+};
+
+function timerIncrement() {
+  idleTime = idleTime + 1;
+  console.log(idleTime);
+  if (idleTime > 9) { // 20 minutes
+      window.location.href = window.location.href.replace("how-does-it-work.html", " ");
+  }
+
+  if (idleTime > 5) {
+    $(".ruthere").css("display", "block");
+  }
+}
